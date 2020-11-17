3320b5964946e2e2f2144ffac4c011446c9e2708
nisisyakure <zm18415@josai.ac.jp>
2020-11-17 02:14:10 +0000

    0707

--- ex06-5.js
+++ ex06-5.js
@@ -3,7 +3,8 @@ function sayhello65() {
     alert('Hello, ' + who65 + '!');
 }
 function taro65() {
-    who65 = '太郎';
+    who65 = '太郎';    
+    alert(who65 +'太郎が来た' + '!');
 }
 function hanako65() {
     who65 = '花子';
