553eb9325013d06eca3d0b593e1a046320970e73
nisisyakure <zm18415@josai.ac.jp>
2021-02-13 13:01:54 +0000

    message

--- ex06-9.js
+++ ex06-9.js
@@ -3,5 +3,5 @@ function txtchange1() {
 }
 
 function imagchange1(){
-    document.getElementById('logo').src = './image/sai.png';
+    document.getElementById('logo').src = './image/zyousaidaigaku.png';
 }
